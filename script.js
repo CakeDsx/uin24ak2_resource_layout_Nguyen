0e6cabd0ee4c4c11f21aab9ce8e218aa81f7485c
CakeDsx <111729620+CakeDsx@users.noreply.github.com>
2024-01-26 06:29:59 +0100

    Update script.js

--- script.js
+++ script.js
@@ -22,24 +22,18 @@ function showCategory(category) {
 document.addEventListener('DOMContentLoaded', function () {
     const navLinks = document.querySelectorAll('#categoryNav a');
 
-    // Function to handle category click
     function handleCategoryClick(event) {
         const selectedCategory = event.target.textContent;
         showCategory(selectedCategory);
 
-        // Remove 'active' class from all links
         navLinks.forEach(link => link.classList.remove('active'));
-        // Add 'active' class to the clicked link
         event.target.classList.add('active');
     }
 
-    // Attach click event to each link
     navLinks.forEach(link => {
         link.addEventListener('click', handleCategoryClick);
     });
 
-    // Show 'HTML' category by default
     showCategory('HTML');
-    // Add 'active' class to the 'HTML' link
     navLinks[0].classList.add('active');
 });
